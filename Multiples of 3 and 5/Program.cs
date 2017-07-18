30126f43976787f98be2c40de8760c9992571563
George Grasham <George Grasham>
2017-07-18 01:25:02 +0100

    Add project files.

---
+++ Multiples of 3 and 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Multiples_of_3_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Limit = 1000;
+            List<int> Numbers = new List<int>();
+            int Sum = 0;
+            for (int i = 0; i < Limit; i++)
+            {
+                if (((i % 3) == 0) || ((i % 5) == 0))
+                {
+                    Numbers.Add(i);
+                }
+            }
+            foreach (int i in Numbers)
+            {
+                Sum += i;        
+            }
+            Console.WriteLine("Count = " + Sum);
+            Console.ReadLine();
+        }
+    }
+}
